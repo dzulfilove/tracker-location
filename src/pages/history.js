efe12b4194ef0c2b8693a6f219bffc135f82dc28
komangchandra <komangchandraaa1@gmail.com>
2024-05-20 09:54:36 +0700

    fix: history perjalanan

--- src/pages/history.js
+++ src/pages/history.js
@@ -2,34 +2,32 @@ import * as React from "react";
 import Navbar from "../components/navbar";
 import { collection, doc, getDocs, query, where } from "firebase/firestore";
 import { db } from "../config/firebase";
+import dayjs from "dayjs";
+import "dayjs/locale/id";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import TextField from "@mui/material/TextField";
+import { DatePicker } from "@mui/x-date-pickers";
+
+dayjs.locale("id");
 
 class History extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tanggalAwal: "2024-05-01",
-      tanggalAkhir: null,
+      tanggalAwal: dayjs("2024-05-01").locale("id"),
+      tanggalAkhir: dayjs().locale("id"),
+      tanggalAwalString: "2024-05-01",
+      tanggalAkhirString: dayjs().locale("id").format("YYYY-MM-DD"),
       displayName: "",
       user: {},
-      currentTrip: {
-        title: "Monitoring GTS",
-        date: "15 Sep, 2022",
-        from: "Kantor Pusat",
-        status: "Belum Sampai",
-      },
       trips: [],
+      filteredTrips: [],
+      isFilter: false,
     };
   }
 
   componentDidMount = async () => {
-    const today = new Date();
-    const year = today.getFullYear();
-    const month = String(today.getMonth() + 1).padStart(2, "0");
-    const day = String(today.getDate()).padStart(2, "0");
-    const tanggalAkhir = `${year}-${month}-${day}`;
-    console.log("tanggalAkhir:", tanggalAkhir);
-    this.setState({ tanggalAkhir });
-
     const userEmail = localStorage.getItem("userEmail");
     await this.setState({ displayName: userEmail });
     await this.getAllHistory();
@@ -48,8 +46,6 @@ class History extends React.Component {
       await new Promise((resolve) => {
         this.setState({ user: userData }, resolve);
       });
-
-      console.log({ user: this.state.user });
 
       return userData;
     } catch (error) {
@@ -99,12 +95,43 @@ class History extends React.Component {
       await new Promise((resolve) => {
         this.setState({ trips: tripList }, resolve);
       });
-
-      console.log({ trips: this.state.trips });
     } catch (error) {
       console.error("Error fetching data: ", error);
       throw error;
     }
+  };
+
+  handleFilter = (name, value) => {
+    const dayjsDate = dayjs(value);
+
+    let tanggalMulai = this.state.tanggalAwalString;
+    let tanggalSelesai = this.state.tanggalAkhirString;
+
+    if (!dayjsDate.isValid()) {
+      return;
+    }
+
+    const formattedDate = dayjsDate.format("YYYY-MM-DD");
+
+    if (name == "tanggalAwal") {
+      tanggalMulai = formattedDate;
+      this.setState({ tanggalAwal: value, tanggalAwalString: formattedDate });
+    } else {
+      tanggalSelesai = formattedDate;
+      this.setState({ tanggalAkhir: value, tanggalAkhirString: formattedDate });
+    }
+
+    const { trips } = this.state;
+
+    const filteredTrips = trips.filter((trip) => {
+      const tripDate = new Date(trip.tanggal);
+      const startDate = new Date(tanggalMulai);
+      const endDate = new Date(tanggalSelesai);
+
+      return tripDate >= startDate && tripDate <= endDate;
+    });
+
+    this.setState({ filteredTrips, isFilter: true });
   };
 
   render() {
@@ -128,59 +155,115 @@ class History extends React.Component {
             </div>
           </div>
           <div className="w-full flex justify-between items-center mt-6">
-            <div className="w-5/12 border bg-blue-500 rounded-md flex justify-center items-center px-2 py-4 text-sm font-medium text-white shadow-md">
-              <input
-                type="date"
-                className="bg-blue-500"
-                value={this.state.tanggalAwal}
-                onChange={(e) => this.setState({ tanggalAwal: e.target.value })}
-              />
+            <div className="w-5/12 flex justify-center items-center px-2 py-4 text-sm font-medium text-white">
+              <LocalizationProvider
+                dateAdapter={AdapterDayjs}
+                adapterLocale="id">
+                <DatePicker
+                  name="tanggalAwal"
+                  locale="id"
+                  label="Tanggal Awal"
+                  value={this.state.tanggalAwal}
+                  onChange={(selectedDate) =>
+                    this.handleFilter("tanggalAwal", selectedDate)
+                  }
+                  inputFormat="DD/MM/YYYY"
+                  renderInput={(params) => (
+                    <TextField {...params} placeholder="dd/mm/yyyy" fullWidth />
+                  )}
+                />
+              </LocalizationProvider>
             </div>
             --
-            <div className="w-5/12 border bg-blue-500 rounded-md flex justify-center items-center px-2 py-4 text-sm font-medium text-white shadow-md">
-              <input
-                type="date"
-                className="bg-blue-500"
-                value={this.state.tanggalAkhir}
-                // onChange={}
-              />
+            <div className="w-5/12 flex justify-center items-center px-2 py-4 text-sm font-medium text-white">
+              <LocalizationProvider
+                dateAdapter={AdapterDayjs}
+                adapterLocale="en-gb">
+                <DatePicker
+                  name="tanggalAkhir"
+                  locale="id"
+                  label="Tanggal Akhir"
+                  value={this.state.tanggalAkhir}
+                  onChange={(selectedDate) =>
+                    this.handleFilter("tanggalAkhir", selectedDate)
+                  }
+                  inputFormat="DD/MM/YYYY"
+                />
+              </LocalizationProvider>
             </div>
           </div>
           <div className=" bg-blue-500 text-white w-full  p-3 rounded-lg flex-auto self-start mt-4 text-base  font-medium">
             Data Perjalanan
           </div>
-          <div className="flex flex-col px-5 mt-6 capitalize bg-slate-50 rounded-2xl  ">
-            {this.state.trips.map((trip) => (
-              <div
-                key={trip.id}
-                onClick={() => {
-                  window.location.href = "/detail-trip";
-                }}
-                className="flex flex-col justify-center mt-8 w-full text-xs font-bold leading-5 capitalize bg-white rounded-2xl ">
-                <div className="flex gap-3.5 px-2 py-2.5 bg-white rounded-2xl border  border-solid shadow-md">
-                  <img
-                    loading="lazy"
-                    srcSet="https://thumb.viva.co.id/media/frontend/thumbs3/2023/08/13/64d80ec78bcc1-cristiano-ronaldo-juara-arab-club-champions-cup-di-al-nassr_1265_711.jpg"
-                    className="shrink-0 aspect-[0.81] w-[30%] rounded-md object-cover"
-                  />
-                  <div className="flex flex-col self-start mt-1.5 w-[65%]">
-                    <div className="text-base leading-5 flex flex-wrap w-full font-medium  ">
-                      {/* {trip.title} */}
-                    </div>
-                    <div className="mt-2 text-sm font-medium leading-5 text-start text-stone-500">
-                      {/* {trip.date} */}
-                    </div>
-                    <div className="mt-2.5 text-start text-blue-500">
-                      {/* {trip.route} */}
-                    </div>
-                    <div className="flex text-sm font-medium justify-center items-start p-2 w-full mt-2.5 text-right text-white bg-blue-500 rounded-lg">
-                      {/* {trip.distance} ({trip.duration}) */}
+          {this.state.isFilter ? (
+            <div className="flex flex-col px-5 mt-6 capitalize bg-slate-50 rounded-2xl  ">
+              {this.state.filteredTrips.map((trip) => (
+                <div
+                  key={trip.id}
+                  onClick={() => {
+                    window.location.href = "/detail-trip";
+                  }}
+                  className="flex flex-col justify-center mt-8 w-full text-xs font-bold leading-5 capitalize bg-white rounded-2xl ">
+                  <div className="flex gap-3.5 px-2 py-2.5 bg-white rounded-2xl border  border-solid shadow-md">
+                    <img
+                      loading="lazy"
+                      srcSet={trip.fotoBukti}
+                      className="shrink-0 aspect-[0.81] w-[30%] rounded-md object-cover"
+                    />
+                    <div className="flex flex-col self-start mt-1.5 w-[65%]">
+                      <div className="text-base leading-5 flex flex-wrap w-full font-medium  ">
+                        {trip.alasan}
+                      </div>
+                      <div className="mt-2 text-sm font-medium leading-5 text-start text-stone-500">
+                        {trip.tanggal}
+                      </div>
+                      <div className="mt-2.5 text-start text-blue-500">
+                        {trip.lokasiAwal[0].lokasi} -{" "}
+                        {trip.lokasiAkhir[0].lokasi}
+                      </div>
+                      <div className="flex text-sm font-medium justify-center items-start p-2 w-full mt-2.5 text-right text-white bg-blue-500 rounded-lg">
+                        {Math.round(trip.jarak)} KM ({trip.durasi} Menit)
+                      </div>
                     </div>
                   </div>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          ) : (
+            <div className="flex flex-col px-5 mt-6 capitalize bg-slate-50 rounded-2xl  ">
+              {this.state.trips.map((trip) => (
+                <div
+                  key={trip.id}
+                  onClick={() => {
+                    window.location.href = "/detail-trip";
+                  }}
+                  className="flex flex-col justify-center mt-8 w-full text-xs font-bold leading-5 capitalize bg-white rounded-2xl ">
+                  <div className="flex gap-3.5 px-2 py-2.5 bg-white rounded-2xl border  border-solid shadow-md">
+                    <img
+                      loading="lazy"
+                      srcSet={trip.fotoBukti}
+                      className="shrink-0 aspect-[0.81] w-[30%] rounded-md object-cover"
+                    />
+                    <div className="flex flex-col self-start mt-1.5 w-[65%]">
+                      <div className="text-base leading-5 flex flex-wrap w-full font-medium  ">
+                        {trip.alasan}
+                      </div>
+                      <div className="mt-2 text-sm font-medium leading-5 text-start text-stone-500">
+                        {trip.tanggal}
+                      </div>
+                      <div className="mt-2.5 text-start text-blue-500">
+                        {trip.lokasiAwal[0].lokasi} -{" "}
+                        {trip.lokasiAkhir[0].lokasi}
+                      </div>
+                      <div className="flex text-sm font-medium justify-center items-start p-2 w-full mt-2.5 text-right text-white bg-blue-500 rounded-lg">
+                        {Math.round(trip.jarak)} KM ({trip.durasi} Menit)
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          )}
         </div>
       </div>
     );
