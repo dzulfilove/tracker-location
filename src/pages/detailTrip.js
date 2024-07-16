f43eee359be026178e80887c70e1c0cdcff617af
dzulfilove <129120910+dzulfilove@users.noreply.github.com>
2024-07-16 13:20:41 +0700

    Update detailTrip.js

--- src/pages/detailTrip.js
+++ src/pages/detailTrip.js
@@ -61,7 +61,7 @@ class DetailTrip extends React.Component {
     };
   }
   componentDidMount = async () => {
-    const userEmail = sessionStorage.getItem("userEmail");
+    const userEmail = localStorage.getItem("userEmail");
     AOS.init({ duration: 700 });
     await this.getUserLogin(userEmail);
     await this.getDataPerjalanan();
