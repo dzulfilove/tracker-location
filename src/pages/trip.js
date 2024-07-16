415dbd5711f39831a0aca3f5e6b217716d36f491
dzulfilove <129120910+dzulfilove@users.noreply.github.com>
2024-07-16 13:22:02 +0700

    Update trip.js

--- src/pages/trip.js
+++ src/pages/trip.js
@@ -25,7 +25,7 @@ class MyTrip extends React.Component {
 
   componentDidMount = async () => {
     this.getHariIni();
-    const userEmail = sessionStorage.getItem("userEmail");
+    const userEmail = localStorage.getItem("userEmail");
     await this.setState({ displayName: userEmail });
     AOS.init({ duration: 700 });
     await this.getAllTripsByUid();
